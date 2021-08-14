283f81b0fa37e6b7145f73d392d31da26b3e373d
amada10 <amada.rktvo@gmail.com>
2021-08-15 00:16:00 +0300

    template ameliore

--- src/components/Footers/Footer.js
+++ src/components/Footers/Footer.js
@@ -8,7 +8,8 @@ export default function Footer() {
         <
         div className = "bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20 h-20"
         style = {
-            { transform: "translateZ(0)" } } >
+            { transform: "translateZ(0)" }
+        } >
         <
         svg className = "absolute bottom-0 overflow-hidden"
         xmlns = "http://www.w3.org/2000/svg"
@@ -19,39 +20,34 @@ export default function Footer() {
         y = "0" >
         <
         polygon className = "text-blueGray-200 fill-current"
-        points = "2560 0 2560 100 0 100" >
-        < /polygon> <
-        /svg> <
-        /div> <
+        points = "2560 0 2560 100 0 100" > < /polygon>  <
+        /svg>  <
+        /div> 
+
+        <
         div className = "container mx-auto px-4" >
         <
         div className = "flex flex-wrap text-center lg:text-left" >
         <
         div className = "w-full lg:w-6/12 px-4" >
         <
-        h4 className = "text-3xl font-semibold" > Let 's keep in touch!</h4> <
-        h5 className = "text-lg mt-0 mb-2 text-blueGray-600" >
-        Suivez - nous sur les réseau sociaux. <
-        /h5> <
+        h4 className = "text-3xl font-semibold" > Let 's keep in touch!</h4>  <
+        h5 className = "text-lg mt-0 mb-2 text-blueGray-600" > Suivez - nous sur les réseau sociaux. < /h5>  <
         div className = "mt-6 lg:mb-0 mb-6" >
         <
         button className = "bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
-        type = "button" >
+        type = "button" > < i className = "fab fa-twitter" > < /i> </button >
+
         <
-        i className = "fab fa-twitter" > < /i> <
-        /button> <
         button className = "bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
-        type = "button" >
+        type = "button" > < i className = "fab fa-facebook-square" > < /i> </button >
+
         <
-        i className = "fab fa-facebook-square" > < /i> <
-        /button> <
         button className = "bg-white text-blueGray-800 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
-        type = "button" >
+        type = "button" > < i className = "fab fa-github" > < /i> </button >
         <
-        i className = "fab fa-github" > < /i> <
-        /button> <
-        /div> <
-        /div> <
+        /div>  <
+        /div>  <
         div className = "w-full lg:w-6/12 px-4" >
         <
         div className = "flex flex-wrap items-top mb-6" >
@@ -59,59 +55,39 @@ export default function Footer() {
         div className = "w-full lg:w-4/12 px-4 ml-auto" >
         <
         span className = "block uppercase text-blueGray-500 text-sm font-semibold mb-2" >
-        Useful Links <
-        /span> <
+        Useful Links < /span>  <
         ul className = "list-unstyled" >
         <
-        li >
+        li > < a className = "text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+        href = "https://iteams.mg" > About Us < /a></li >
+
         <
-        a className = "text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-        href = "https://iteams.mg" >
-        About Us <
-        /a> <
-        /li> <
-        li >
+        li > < a className = "text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+        href = "https://iteams.mg" > Page < /a> </li >
+
         <
-        a className = "text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-        href = "https://iteams.mg" >
-        Page <
-        /a> <
-        /li> <
-        li >
+        li > < a className = "text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+        href = "https://iteams.mg" > Github < /a> </li >
+
         <
-        a className = "text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-        href = "https://iteams.mg" >
-        Github <
-        /a> <
-        /li> <
-        li >
+        li > < a className = "text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+        href = "https://iteams.mg" > Products < /a> </li >
         <
-        a className = "text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-        href = "https://iteams.mg" >
-        Products <
-        /a> <
-        /li> <
-        /ul> <
-        /div> <
+        /ul>  <
+        /div>  <
         div className = "w-full lg:w-4/12 px-4" >
         <
-        span className = "block uppercase text-blueGray-500 text-sm font-semibold mb-2" >
-        Other Resources <
-        /span> <
+        span className = "block uppercase text-blueGray-500 text-sm font-semibold mb-2" > Other Resources < /span>  <
         ul className = "list-unstyled" >
         <
-        li >
+        li > < a className = "text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+        href = "https://iteams.mg" > Contact Us < /a> </li >
         <
-        a className = "text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-        href = "https://iteams.mg" >
-        Contact Us <
-        /a> <
-        /li> <
-        /ul> <
-        /div> <
-        /div> <
-        /div> <
-        /div> <
+        /ul>  <
+        /div>  <
+        /div>  <
+        /div>  <
+        /div>  <
         hr className = "my-6 border-blueGray-300" / >
         <
         div className = "flex flex-wrap items-center md:justify-between justify-center" >
@@ -120,17 +96,13 @@ export default function Footer() {
         <
         div className = "text-sm text-blueGray-500 font-semibold py-1" >
         Copyright© { new Date().getFullYear() } { " " }
-        by { " " } <
-        a href = "https://iteams.mg"
-        className = "text-blueGray-500 hover:text-blueGray-800" >
-        Iteam - $ <
-        /a>
-        . <
-        /div> <
-        /div> <
-        /div> <
-        /div> <
-        /footer> <
+        by { " " } < a href = "https://iteams.mg"
+        className = "text-blueGray-500 hover:text-blueGray-800" > Iteam - $ < /a> <
+        /div>  <
+        /div>  <
+        /div>  <
+        /div>  <
+        /footer>  <
         />
     );
 }
