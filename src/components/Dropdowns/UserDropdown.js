7d3cec3897f080d2f56fa7745388a29e0c9de1ef
amada10 <diamondrap20.aps1b@gmail.com>
2021-08-28 19:25:56 +0300

    formulaire projet et amelioration code jsx

--- src/components/Dropdowns/UserDropdown.js
+++ src/components/Dropdowns/UserDropdown.js
@@ -15,72 +15,48 @@ const UserDropdown = () => {
     const closeDropdownPopover = () => {
         setDropdownPopoverShow(false);
     };
-    return ( <
-        >
-        <
-        a className = "text-blueGray-500 block"
-        href = "#pablo"
-        ref = { btnDropdownRef }
-        onClick = {
-            (e) => {
-                e.preventDefault();
-                dropdownPopoverShow ? closeDropdownPopover() : openDropdownPopover();
-            }
-        } >
-        <
-        div className = "items-center flex" >
-        <
-        span className = "w-12 h-12 text-sm text-white bg-blueGray-200 inline-flex items-center justify-center rounded-full" >
-        <
-        img alt = "..."
-        className = "w-full rounded-full align-middle border-none shadow-lg"
-        src = { require("assets/img/team-1-800x800.jpg").default }
-        /> <
-        /span> <
-        /div> <
-        /a> <
-        div ref = { popoverDropdownRef }
-        className = {
-            (dropdownPopoverShow ? "block " : "hidden ") +
-            "bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48"
-        } >
-        <
-        a href = "#pablo"
-        className = {
-            "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
-        }
-        onClick = {
-            (e) => e.preventDefault() } >
-        Mon Profil <
-        /a> <
-        a href = "#pablo"
-        className = {
-            "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
-        }
-        onClick = {
-            (e) => e.preventDefault() } >
-        Parametre <
-        /a> <
-        a href = "#pablo"
-        className = {
-            "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
-        }
-        onClick = {
-            (e) => e.preventDefault() } >
-        View Profil <
-        /a> <
-        div className = "h-0 my-2 border border-solid border-blueGray-100" / >
-        <
-        a href = "#pablo"
-        className = {
-            "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
-        }
-        onClick = {
-            (e) => e.preventDefault() } >
-        Deconnexion <
-        /a> <
-        /div> <
-        />
+    return ( 
+        <>
+            <a className = "text-blueGray-500 block" href = "#pablo" ref = { btnDropdownRef }
+                onClick = {
+                    (e) => {
+                        e.preventDefault();
+                        dropdownPopoverShow ? closeDropdownPopover() : openDropdownPopover();
+                    }} >
+                <div className = "items-center flex" >
+                    <span className = "w-12 h-12 text-sm text-white bg-blueGray-200 inline-flex items-center justify-center rounded-full" >
+                        <img alt = "..." className = "w-full rounded-full align-middle border-none shadow-lg" src = { require("assets/img/team-1-800x800.jpg").default }/> 
+                    </span> 
+                </div> 
+            </a> 
+            <div ref = { popoverDropdownRef } className = {
+                (dropdownPopoverShow ? "block " : "hidden ") +
+                "bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48"} >
+                    <a href = "#pablo" className = {
+                        "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+                        }
+                        onClick = {
+                            (e) => e.preventDefault() } >Mon Profil 
+                    </a> 
+                    <a href = "#pablo" className = {"text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"}
+                        onClick = {
+                            (e) => e.preventDefault() } >Parametre 
+                    </a> 
+                    <a href = "#pablo" className = {
+                        "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+                        }
+                        onClick = {
+                            (e) => e.preventDefault() } >View Profil 
+                    </a> 
+                    <div className = "h-0 my-2 border border-solid border-blueGray-100" />
+                    <a href = "#pablo" className = {
+                            "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+                                }
+                                onClick = {
+                                    (e) => e.preventDefault() } >Deconnexion 
+                    </a> 
+            </div> 
+        </>
     );
 };
 
