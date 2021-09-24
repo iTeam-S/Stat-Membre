695ca5e9ad219ebc72395bf38321ef10ba0a2926
amada10 <diamondrap20.aps1b@gmail.com>
2021-09-25 00:06:49 +0300

    component for project

--- src/views/Index.js
+++ src/views/Index.js
@@ -43,16 +43,34 @@ export default function Index() {
                 <div className = "container mx-auto" >
                     <div className = "flex flex-wrap items-center" >
                         <div className = "w-10/12 md:w-6/12 lg:w-4/12 px-12 md:px-4 mr-auto ml-auto -mt-32" >
-                            <div className = "relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg" >
-                                <img alt = "..." src = "https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"
-                                        className = "w-full align-middle rounded-t-lg"/>
-                                <blockquote className = "relative p-8 mb-4" >
-                                    <svg preserveAspectRatio = "none" xmlns = "http://www.w3.org/2000/svg" viewBox = "0 0 583 95" className = "absolute left-0 w-full block h-95-px -top-94-px" >
-                                        <polygon points = "-30,95 583,95 583,65" className = "text-lightBlue-500 fill-current" ></polygon> 
-                                    </svg> 
-                                    <h4 className = "text-xl font-bold text-white" >Great for your awesome project </h4> 
-                                    <p className = "text-md font-light mt-2 text-white" >Putting together a page has never been easier than matching together pre - made components.From landing pages presentation to login areas, you can easily customise and built your pages. </p> 
-                                </blockquote> 
+                            <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-lightBlue-500">
+                                <img
+                                  alt="..."
+                                  src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"
+                                  className="w-full align-middle rounded-t-lg"
+                                />
+                                <blockquote className="relative p-8 mb-4">
+                                  <svg
+                                    preserveAspectRatio="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 583 95"
+                                    className="absolute left-0 w-full block h-95-px -top-94-px"
+                                  >
+                                    <polygon
+                                      points="-30,95 583,95 583,65"
+                                      className="text-lightBlue-500 fill-current"
+                                    ></polygon>
+                                  </svg>
+                                  <h4 className="text-xl font-bold text-white">
+                                    Great for your awesome project
+                                  </h4>
+                                  <p className="text-md font-light mt-2 text-white">
+                                    Putting together a page has never been easier than matching
+                                    together pre-made components. From landing pages
+                                    presentation to login areas, you can easily customise and
+                                    built your pages.
+                                  </p>
+                                </blockquote>
                             </div> 
                         </div>
 
@@ -104,7 +122,160 @@ export default function Index() {
                 </div> 
             </section>
 
-            <section className = "block relative z-1 bg-blueGray-600" >
+            <div className="flex flex-wrap items-center pt-32 pb-64">
+                <div className="w-full md:w-6/12 px-4 mr-auto ml-auto mt-32">
+                    <div className="justify-center flex flex-wrap relative">
+                        <div className="my-4 w-full lg:w-6/12 px-4">
+                                <a
+                                    href="https://www.creative-tim.com/learning-lab/tailwind/svelte/alerts/notus?ref=vtw-index"
+                                    target="_blank"
+                                >
+                                    <div className="bg-red-600 shadow-lg rounded-lg text-center p-8">
+                                      <img
+                                        alt="..."
+                                        className="shadow-md rounded-full max-w-full w-16 mx-auto p-2 bg-white"
+                                        src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/logos/svelte.jpg"
+                                      />
+                                      <p className="text-lg text-white mt-4 font-semibold">
+                                        Svelte
+                                      </p>
+                                    </div>
+                                </a>
+                                <a
+                                    href="https://www.creative-tim.com/learning-lab/tailwind/react/alerts/notus?ref=vtw-index"
+                                    target="_blank"
+                                >
+                                    <div className="bg-lightBlue-500 shadow-lg rounded-lg text-center p-8 mt-8">
+                                      <img
+                                        alt="..."
+                                        className="shadow-md rounded-full max-w-full w-16 mx-auto p-2 bg-white"
+                                        src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/logos/react.jpg"
+                                      />
+                                      <p className="text-lg text-white mt-4 font-semibold">
+                                        ReactJS
+                                      </p>
+                                    </div>
+                                </a>
+                                <a
+                                    href="https://www.creative-tim.com/learning-lab/tailwind/nextjs/alerts/notus?ref=vtw-index"
+                                    target="_blank"
+                                >
+                                    <div className="bg-blueGray-700 shadow-lg rounded-lg text-center p-8 mt-8">
+                                      <img
+                                        alt="..."
+                                        className="shadow-md rounded-full max-w-full w-16 mx-auto p-2 bg-white"
+                                        src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/logos/nextjs.jpg"
+                                      />
+                                      <p className="text-lg text-white mt-4 font-semibold">
+                                        NextJS
+                                      </p>
+                                    </div>
+                                </a>
+                        </div>
+                        <div className="my-4 w-full lg:w-6/12 px-4 lg:mt-16">
+                            <a
+                                href="https://www.creative-tim.com/learning-lab/tailwind/js/alerts/notus?ref=vtw-index"
+                                target="_blank"
+                            >
+                                <div className="bg-yellow-500 shadow-lg rounded-lg text-center p-8">
+                                  <img
+                                    alt="..."
+                                    className="shadow-md rounded-full max-w-full w-16 mx-auto p-2 bg-white"
+                                    src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/logos/js.png"
+                                  />
+                                  <p className="text-lg text-white mt-4 font-semibold">
+                                    JavaScript
+                                  </p>
+                                </div>
+                            </a>
+                            <a
+                                href="https://www.creative-tim.com/learning-lab/tailwind/angular/alerts/notus?ref=vtw-index"
+                                target="_blank"
+                            >
+                                <div className="bg-red-700 shadow-lg rounded-lg text-center p-8 mt-8">
+                                  <img
+                                    alt="..."
+                                    className="shadow-md rounded-full max-w-full w-16 mx-auto p-2 bg-white"
+                                    src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/logos/angular.jpg"
+                                  />
+                                  <p className="text-lg text-white mt-4 font-semibold">
+                                    Angular
+                                  </p>
+                                </div>
+                            </a>
+                            <a
+                                href="https://www.creative-tim.com/learning-lab/tailwind/vue/alerts/notus?ref=vtw-index"
+                                target="_blank"
+                            >
+                                <div className="bg-emerald-500 shadow-lg rounded-lg text-center p-8 mt-8">
+                                  <img
+                                    alt="..."
+                                    className="shadow-md rounded-full max-w-full w-16 mx-auto p-2 bg-white"
+                                    src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/logos/vue.jpg"
+                                  />
+                                  <p className="text-lg text-white mt-4 font-semibold">
+                                    Vue.js
+                                  </p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="w-full md:w-4/12 px-12 md:px-4 ml-auto mr-auto mt-48">
+                    <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white">
+                        <i className="fas fa-drafting-compass text-xl"></i>
+                    </div>
+                    <h3 className="text-3xl mb-2 font-semibold leading-normal">
+                        Javascript Components
+                    </h3>
+                    <p className="text-lg font-light leading-relaxed mt-4 mb-4 text-blueGray-600">
+                        In order to create a great User Experience some components
+                        require JavaScript. In this way you can manipulate the elements
+                        on the page and give more options to your users.
+                    </p>
+                    <p className="text-lg font-light leading-relaxed mt-4 mb-4 text-blueGray-600">
+                        We created a set of Components that are dynamic and come to help
+                        you.
+                    </p>
+                    <div className="block pb-6">
+                        <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-white uppercase last:mr-0 mr-2 mt-2">
+                            Alerts
+                        </span>
+                        <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-white uppercase last:mr-0 mr-2 mt-2">
+                            Dropdowns
+                        </span>
+                        <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-white uppercase last:mr-0 mr-2 mt-2">
+                          Menus
+                        </span>
+                        <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-white uppercase last:mr-0 mr-2 mt-2">
+                          Modals
+                        </span>
+                        <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-white uppercase last:mr-0 mr-2 mt-2">
+                          Navbars
+                        </span>
+                        <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-white uppercase last:mr-0 mr-2 mt-2">
+                          Popovers
+                        </span>
+                        <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-white uppercase last:mr-0 mr-2 mt-2">
+                          Tabs
+                        </span>
+                        <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-white uppercase last:mr-0 mr-2 mt-2">
+                          Tooltips
+                        </span>
+                    </div>
+                    <a
+                        href="https://www.creative-tim.com/learning-lab/tailwind/react/alerts/notus?ref=nr-index"
+                        target="_blank"
+                        className="font-bold text-blueGray-700 hover:text-blueGray-500 ease-linear transition-all duration-150"
+                    >
+                        View all{" "}
+                        <i className="fa fa-angle-double-right ml-1 leading-relaxed"></i>
+                    </a>
+                </div>
+            </div>
+
+            <section className = "block relative z-1 bg-blueGray-600 pb-48" >
                 <div className = "container mx-auto" >
                     <div className = "justify-center flex flex-wrap" >
                         <div className = "w-full lg:w-12/12 px-4  -mt-24" >
@@ -141,9 +312,7 @@ export default function Index() {
                 </div> 
             </section>
 
-            <section className = "py-10 bg-blueGray-600 overflow-hidden" >
-                <div className = "container mx-auto pb-64" ></div> 
-            </section> 
+            
             <Footer/>
         </>
     );
