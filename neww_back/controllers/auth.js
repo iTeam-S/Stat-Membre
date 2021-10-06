6a32682eb480599bf5282703d514751ad128c3c9
hasina821 <80751503+hasina821@users.noreply.github.com>
2021-10-06 23:00:13 +0200

    update auth

--- neww_back/controllers/auth.js
+++ neww_back/controllers/auth.js
@@ -1,17 +1,20 @@
 const db = require('../service/connect');
 const mdlsUser=require("../models/users");
 
-var jwt=reqquire
-var bcrypt=require("bcryptjs")
-1
-
+var jwt=require("jsonwebtoken")
+var bcrypt=require("bcryptjs");
 
 module.exports={
     signUp:async(req,res)=>{
         try {
-            let nom=req.body.nom,mail=req.body.mail,hashPassword=bcrypt.hashSync(req.body.password,8);
-            let user=await mdlsUser.create(nom,mail,hashPassword);
-            res.status(200).send(user);
+            let nom=req.body.prenom;mail=req.body.email,hashPassword=bcrypt.hashSync(req.body.password,8);
+
+            
+            let useR=await mdlsUser.create(nom,mail,hashPassword);
+          
+
+            res.json(useR.rows[0]);
+            
             
         } catch (error) {
             res.send(error)
@@ -19,7 +22,43 @@ module.exports={
         }
     },
     signin:async(req,res)=>{
+        try {
+            let nom=req.body.nom;
+            let useR=await mdlsUser.getUserWithRole(nom);
+
+            if(!useR){
+                return res.status(404).send({
+                    message:"User Not found."
+                })
+            }
+            var passwordIsValid=bcrypt.compareSync(
+                    req.body.password,
+                    useR.user_password
+
+                );
+                if(!passwordIsValid){
+                    return res.status(401).send({
+                        accessToken:null,
+                        message:"Invalid password"
+                    });
+                }
+            var token =jwt.sign({id:useR.id},config.secret,{
+                expiresIn:86400
+            });
+            res.status(200).send({
+                id:useR.id,
+                username:useR.username,
+                email:useR.user_mail,
+                roles:useR.user_role,
+                accessToken:token
+            });
+            
+        } catch (error) {
+            res.status(500).send(error)
+            
+        }
 
     }
 }
+
 
