e8a1a871943187f728f12a55f4addcfcba198b66
hasina821 <80751503+hasina821@users.noreply.github.com>
2021-10-06 23:08:02 +0200

    update auth

--- neww_back/app.js
+++ neww_back/app.js
@@ -7,6 +7,10 @@ const app = express();
 const db = require('./service/connect');
 
 app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Headers",
+    "x-access-token, Origin, Content-Type, Accept"
+  );
   //d'accéder à notre API depuis n'importe quelle origine ( '*' ) ;
   res.setHeader('Access-Control-Allow-Origin', '*');
   //d'ajouter les headers mentionnés aux requêtes envoyées vers notre API (Origin , X-Requested-With , etc.) ;
@@ -26,9 +30,9 @@ app.use('/api/member',MembersRoutes );
 const CriteresRoutes = require('./routes/critere');
 app.use('/api/critere',CriteresRoutes );
 
-/*const userRoutes = require('./routes/user');
+const userRoutes = require('./routes/authroutes');
 app.use('/api/auth', userRoutes);
-*/
+
 
 const ProjectsRoutes = require('./routes/project');
 app.use('/api/project', ProjectsRoutes);
