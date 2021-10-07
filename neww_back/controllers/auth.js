ec07ea769b41ac1281c6651353034e63608aafb2
hasina821 <80751503+hasina821@users.noreply.github.com>
2021-10-07 23:03:29 +0200

    Update auth.js

--- neww_back/controllers/auth.js
+++ neww_back/controllers/auth.js
@@ -1,64 +1,77 @@
 const db = require('../service/connect');
-const mdlsUser=require("../models/users");
+const mdlsUser = require("../models/users");
 
-var jwt=require("jsonwebtoken")
-var bcrypt=require("bcryptjs");
+var jwt = require("jsonwebtoken")
+var bcrypt = require("bcryptjs");
 
-module.exports={
-    signUp:async(req,res)=>{
+module.exports = {
+    signUp: async (req, res) => {
         try {
-            let nom=req.body.prenom;mail=req.body.email,hashPassword=bcrypt.hashSync(req.body.password,8);
+            let nom = req.body.prenom; mail = req.body.email, hashPassword = bcrypt.hashSync(req.body.password, 8);
 
-            
-            let useR=await mdlsUser.create(nom,mail,hashPassword);
-          
-
-            res.json(useR.rows[0]);
-            
-            
-        } catch (error) {
-            res.send(error)
-            
-        }
-    },
-    signin:async(req,res)=>{
-        try {
-            let nom=req.body.nom;
-            let useR=await mdlsUser.getUserWithRole(nom);
-
-            if(!useR){
-                return res.status(404).send({
-                    message:"User Not found."
+            let userBm=await mdlsUser.checkUserByMail(mail)
+            let userBn=await mdlsUser.checkUserByName(nom)
+           
+            if(userBm.rows[0] || userBn.rows[0]){
+                res.send({
+                    message:"Email or username already in use"
+                })
+                console.log(userBm.rows[0]);
+            }else if(userBn.rows[0] && userBm.rows[0]){
+                res.send({
+                    message:"This account is already exist,please use other mail and username"
+                })
+            }else{
+                let useR =await mdlsUser.create(nom,mail,hashPassword);
+                res.status(200).send({
+                    message:"Created a new account with success"
                 })
             }
-            var passwordIsValid=bcrypt.compareSync(
-                    req.body.password,
-                    useR.user_password
+        } catch (error) {
+            res.send(error)
 
-                );
-                if(!passwordIsValid){
-                    return res.status(401).send({
-                        accessToken:null,
-                        message:"Invalid password"
-                    });
-                }
-            var token =jwt.sign({id:useR.id},config.secret,{
-                expiresIn:86400
+        }
+    },
+    signin: async (req, res) => {
+        try {
+            let prenom = req.body.prenom;
+            let useR = await mdlsUser.getUserWithRoles(prenom);
+
+            if (!useR) {
+                return res.status(404).send({
+                    message: "User Not found."
+                })
+            }
+            var passwordIsValid = bcrypt.compareSync(
+                req.body.password,
+                useR.user_password
+
+            );
+            if (!passwordIsValid) {
+                return res.status(401).send({
+                    accessToken: null,
+                    message: "Invalid password"
+                });
+            }
+            var token = jwt.sign({ id: useR.id }, config.secret, {
+                expiresIn: 86400
             });
             res.status(200).send({
-                id:useR.id,
-                username:useR.username,
-                email:useR.user_mail,
-                roles:useR.user_role,
-                accessToken:token
+                id: useR.id,
+                username: useR.username,
+                email: useR.user_mail,
+                roles: useR.user_role,
+                accessToken: token
             });
-            
+
         } catch (error) {
             res.status(500).send(error)
-            
+
         }
 
     }
 }
+
+
 
 
