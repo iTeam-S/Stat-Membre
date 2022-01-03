5fce297864ba7685741f95d1cada067052e2483e
hasina821 <80751503+hasina821@users.noreply.github.com>
2022-01-03 21:40:25 +0000

    Update connect.js

--- back-end/service/connect.js
+++ back-end/service/connect.js
@@ -4,11 +4,10 @@ const Pool=require("pg").Pool;
 
 
 const db=new Pool({
-    host: "localhost",
-    database: "iteams",
-    password: "vltn21/m",
-    user: "postgres",
-    port: 5432
+   host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 })
 
 db.connect((err) => {
