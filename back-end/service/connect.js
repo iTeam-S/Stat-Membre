90fd057d3e1cf020a323ec914d8c7cd4e6ff4b87
amada10 <amada.rktvo@gmail.com>
2022-01-05 20:05:24 +0300

    modif connect.js

--- back-end/service/connect.js
+++ back-end/service/connect.js
@@ -3,11 +3,10 @@ require('dotenv').config()
 const Pool=require("pg").Pool;
 
 const db=new Pool({
-    host: '127.0.0.1',
-    database: 'iteams',
-    password: '1404',
-    user: 'postgres',
-    port: 5432,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 })
 
 db.connect((err) => {
