9c1785a25c5c41da09f2705009f28bb80451474d
amada10 <amada.rktvo@gmail.com>
2022-01-23 18:48:22 +0300

    rectif

--- back-end/service/connect.js
+++ back-end/service/connect.js
@@ -1,18 +1,18 @@
 require('dotenv').config()
+const { Pool } = require('pg')
 
 
-
-const mysql = require("mysql");
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+const db = new Pool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'iteams',
+    port: 3306,
 });
 
 db.connect((err) => {
   if (err) {
-    console.log("Errer lors du connexion à la base de donnee");
+    console.log("Erreur lors du connexion à la base de donnee");
     return;
   }
   console.log("Connexion à la base de donnée reussie");
--- front-end/src/pages/layouts/Checkmproject.js
+++ front-end/src/pages/layouts/Checkmproject.js
@@ -3,15 +3,14 @@ import React from "react";
 // components
 
 import Navbar from "../../components/Navbars/AuthNavbar.js";
-import FooterSmall from "../../components/Footers/FooterSmall.js";
+
 
 // views
 
 
-import Projectmember from "../admin/ProjectMember"
+import CheckMemberProject from "../admin/CheckMemberProject"
 
-
-export default function ProjectMember() {
+export default function CheckProject() {
   return (
     <>
       <Navbar transparent />
@@ -19,13 +18,8 @@ export default function ProjectMember() {
         <section className="relative w-full h-full py-40 min-h-screen">
           <div
             className="absolute top-0 w-full h-full bg-blueGray-800 bg-no-repeat bg-full"
-            style={{
-              backgroundImage:
-                "url(" + require("assets/img/register_bg_2.png").default + ")",
-            }}
           ></div>
-            <Projectmember/>
-          <FooterSmall absolute />
+          <CheckMemberProject/>
         </section>
       </main>
     </>
