204890b40e203c6e06cb82aaf6513484af8239c9
Gaetan Jonathan <gaetan.s118@gmail.com>
2022-03-05 23:45:27 +0300

    change back url

--- front-end/src/utils/apis/Stat.js
+++ front-end/src/utils/apis/Stat.js
@@ -2,17 +2,17 @@ import Axios from "axios"
 
 
 const ProjectAxios=Axios.create({
-    baseURL:"http://localhost:8000/api/v1/project"
+    baseURL:"https://stat-back.iteam-s.mg/api/v1/project"
 })
 const MemberAxios=Axios.create({
-    baseURL:"http://localhost:8000/api/v1/member"
+    baseURL:"https://stat-back.iteam-s.mg/api/v1/member"
 })
 
 const CritereAxios=Axios.create({
-    baseURL:"http://localhost:8000/api/v1/critere"
+    baseURL:"https://stat-back.iteam-s.mg/api/v1/critere"
 }) 
 const AuthAxios=Axios.create({
-    baseURL:"http://localhost:8000/api/v1/auth"
+    baseURL:"https://stat-back.iteam-s.mg/api/v1/auth"
 })
 const GithubAxios=Axios.create({
     baseURL:"https://github.iteam-s.mg"
